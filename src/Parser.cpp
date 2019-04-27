b05368d981bb350753e3d686527e935b03e4aa8d
Juliana Mayumi Hosoume <ju_hosoume@gmail.com>
2019-04-27 18:33:32 -0300

    update parser.cpp

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -4,6 +4,14 @@ Parser::Parser(const InstructionTable &inst_t, const DirectiveTable &dir_t)
   : instruction_table{inst_t}, directive_table{dir_t}
 {}
 
-bool isExpressionValid(const vector<Token> &tokens) {
+bool Parser::isExpressionValid(const vector<Token> &tokens) {
   return true;
+}
+
+bool Parser::checkNumOfLabels(const vector<Token> &tokens) {
+  int num_labels = 0;
+  for (const Token &token : tokens) {
+    if(token.type == TokenType::LABEL_COLON)
+  }
+
 }
